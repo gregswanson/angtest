ab156d81f1dcf07f578a32184091025f82ef4fb2
Gregory Swanson <gswanson@Gregorys-MacBook-Pro.local>
2017-02-16 15:45:52 -0500

    factory

---
+++ app2.js
@@ -0,0 +1,90 @@
+(function(){
+angular.module('shopApp', [])
+.controller('shoppingListCtrl1', shoppingListCtrl1)
+.controller('shoppingListCtrl2', shoppingListCtrl2)
+.factory('shoppingListFactory', shoppingListFactory);
+
+//list 1 controller
+shoppingListCtrl1.$inject = ['shoppingListFactory'];
+function shoppingListCtrl1(shoppingListFactory) {
+	var list1 = this;
+
+	//use factory to create new shopping list service
+	var shoppingList = shoppingListFactory();
+
+	list1.items = shoppingList.getItems();
+
+	list1.itemName = "";
+	list1.itemQuantity = "";
+
+	list1.addItem = function(){
+		shoppingList.addItem(list1.itemName, list1.itemQuantity);
+		console.log(list1.items);
+	};
+	list1.removeItem = function(itemIndex){
+		shoppingList.removeItem(itemIndex);
+	};
+};
+
+//list 2 controller
+shoppingListCtrl2.$inject =['shoppingListFactory'];
+function shoppingListCtrl2(shoppingListFactory){
+	var list2 = this;
+
+	//use factory to create new shopping list service
+	var shoppingList = shoppingListFactory(3);
+
+	list2.items = shoppingList.getItems();
+
+	list2.itemName = "";
+	list2.itemQuantity = "";
+
+	list2.addItem = function(){
+		try{
+			shoppingList.addItem(list2.itemName, list2.itemQuantity);
+		} catch (error) {
+			list2.errorMessage = error.message;
+		}
+	};
+	list2.removeItem = function(itemIndex){
+		shoppingList.removeItem(itemIndex);
+	};
+}
+
+//if not specified items are assumed unlimited
+function shoppingListService(maxItems){
+	var service = this;
+
+	//list of items
+	var items = [];
+
+	service.addItem = function(itemName, quantity) {
+		if((maxItems === undefined) || (maxItems !== undefined) && (items.length < maxItems)){
+			var item = {
+				name: itemName,
+				quantity: quantity
+			};
+			items.push(item);
+		} else {
+			throw new Error("Max items (" + maxItems + ") reached!");
+		}
+	};
+
+	service.removeItem = function (itemIndex) {
+		items.splice(itemIndex, 1);
+	};
+
+	service.getItems = function() {
+		return items;
+	};
+}
+
+function shoppingListFactory() {
+	var factory = function(maxItems) {
+		return new shoppingListService(maxItems)
+	};
+
+	return factory;
+}
+
+})();
